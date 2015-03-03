2563c88cab8cc030dbde476cab999e9eca654d82
Quark Ng <qn.git@outlook.com>
2015-03-03 09:56:45 -0600

    Initial commit.

---
+++ DataVisualization_001/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.DataVisualization.Charting;
+
+
+// Quick Notes:
+// http://www.codeproject.com/Articles/196502/WPF-Toolkit-Charting-Controls-Line-Bar-Area-Pie-Co
+// https://wpf.codeplex.com/releases/view/40535
+// https://github.com/dotnetprojects/wpftoolkit
+//
+// Newer? https://www.nuget.org/packages/DotNetProjectsWPFToolkit/
+
+namespace DataVisualization_001
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static SampleDataVM sdvm = new SampleDataVM();
+        private Dictionary<string, int> Data = sdvm.Data;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            colChart.ItemsSource = Data;
+        }
+
+        private void btnBar1_Click(object sender, RoutedEventArgs e)
+        {
+            barChart.ItemsSource = Data;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# WPFToolkit.DataVisualization_001 
+Experiments with WPF Toolkit Data Visualization 3.5.50211.1 
+
+----
+### Project Setup Note
+Tools >> NuGet Package Manager >> Package Manager Console
+```
+PM> Install-Package WPFToolkit 
+PM> Install-Package WPFToolkit.DataVisualization 
+```
